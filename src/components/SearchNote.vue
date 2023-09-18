7fdb0c18506b81b5f376f88264a073ec2c2792e4
Ezra Ghaziafi <ezraghzf@gmail.com>
2023-09-18 15:18:51 +0700

    Update texts

--- src/components/SearchNote.vue
+++ src/components/SearchNote.vue
@@ -14,7 +14,7 @@ const updateValue = (event) => {
 <template>
     <div class="w-full h-full bg-slate-100 rounded-lg p-4 inline-flex flex-row gap-2">
         <MagnifyingGlassIcon class="w-6 text-black"></MagnifyingGlassIcon>
-        <input placeholder="Title" class="w-full bg-transparent focus:outline-none" type="search" name="search-note"
+        <input placeholder="Search title" class="w-full bg-transparent focus:outline-none" type="search" name="search-note"
             id="search-note" :value="modelValue" @input="updateValue">
     </div>
 </template>
